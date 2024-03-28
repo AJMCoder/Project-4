6a7b334604dc492ef84a8b7d4c78c2d641542c0c
Alexander Mullins <alexmullins@hotmail.co.uk>
2024-03-28 11:01:21 +0000

    Add a emailjs service to the contact area and create the ability for users to enquire and message.

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("cityserve", "cityserve", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "league_enquiry": contactForm.subject.value,
+        "message": contactForm.message.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            alert("Your email has been sent successfully!");
+        },
+        function(error) {
+            console.log("FAILED", error);
+            alert("Your email was not sent. Please try again later.");
+        }
+    );
+    return false;  // To block from loading a new page
+}
