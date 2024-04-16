e622cf8d865c495755e20d501250f12a450e1a00
Al Mullins <alexmullins@hotmail.co.uk>
2024-04-16 21:40:06 +0000

    added event_detail view

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic 
+from django.views.generic import DetailView
 from .models import Event
 
 # Create your views here.
@@ -9,3 +10,11 @@ class EventList(generic.ListView):
     template_name = "events/index.html"
     context_object_name = "events"
     paginate_by = 6
+
+class EventDetail(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+    context_object_name = 'event'
+
+    def get_object(self):
+        return get_object_or_404(Event, slug=self.kwargs['slug'])
