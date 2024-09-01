a5c9239e4666533317a4ca7fd82935fc46cbf966
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-01 19:16:16 +0000

    Fixed navbar links as the website name was linking to events page. Add feature that removes the login/logout message from top of the page.

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.EventList.as_view(), name="home"),
+    path("", views.EventList.as_view(), name="events"),
     path('event/<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
 ]
